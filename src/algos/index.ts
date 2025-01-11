29fd731b5bcbb82ef165b35a898c0c291a86a5fa
Lukas Fülling <lukas@k40s.net>
2025-01-11 10:05:23 +0100

    add no_image and with_image algos

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -1,11 +1,15 @@
 import { AppContext } from '../AppContext';
 import { OutputSchema as AlgoOutput, QueryParams } from '../lexicon/types/app/bsky/feed/getFeedSkeleton';
 import * as new_posts from './new_posts';
+import * as with_image from './with_image';
+import * as no_image from './no_image';
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>;
 
 const algos: Record<string, AlgoHandler> = {
   [new_posts.shortname]: new_posts.handler,
+  [with_image.shortname]: with_image.handler,
+  [no_image.shortname]: no_image.handler,
 };
 
 export default algos;
